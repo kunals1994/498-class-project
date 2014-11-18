240c23bf922df3c50a6c4e5aea13ee5664a21121
Kunal Sharma <ksharma8@terpmail.umd.edu>
2014-11-18 18:49:11 -0500

    updated Tick Model

--- kensoDataStore/models.py
+++ kensoDataStore/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Tick(models.Model):
+	year = models.IntegerField()
+	month = models.IntegerField()
+	day = models.IntegerField()
+	volume = models.IntegerField()
+	day_open = models.DecimalField(max_digits = 12, decimal_places = 3)
+	percent_change = models.DecimalField(max_digits = 9, decimal_places = 8)
