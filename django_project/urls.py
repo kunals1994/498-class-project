0646989ce507e4fb3b852316aa3e133785d101d1
Kunal Sharma <ksharma8@terpmail.umd.edu>
2014-11-20 03:17:51 -0500

    temporary samples

--- django_project/urls.py
+++ django_project/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'django_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    
     url(r'^api/seeCorrelation', 'kensoDataStore.views.display_volatility', name='cor_api'),
     url(r'^api/getData', 'kensoDataStore.views.get_data', name='raw_data'),
+    url(r'^', 'kensoDataStore.views.temp_home', name="samp"),
 )
