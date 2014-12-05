3007a77a133cab894cfe1855e8e534a2573959c9
Kunal Sharma <ksharma8@terpmail.umd.edu>
2014-12-05 03:49:17 -0500

    new models

--- kensoDataStore/models.py
+++ kensoDataStore/models.py
@@ -10,3 +10,14 @@ class Tick(models.Model):
 
 	def __str__(self):
 		return self.symbol + " : " + str(self.date)
+
+
+class Volitility(models.Model):
+	symbol = models.CharField(max_length = 5)
+	year = models.IntegerField(max_digits = 5)
+	volitliity = models.DecimalField(max_digits = 9, decimal_places = 8)
+
+	def __str__(self):
+		return self.symbol + " : " + str(self.year)
+
+
