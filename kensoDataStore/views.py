3d5a4d8252acdc1800bbc5e831cd294a31d09c02
Kunal Sharma <ksharma8@terpmail.umd.edu>
2014-12-05 19:17:53 -0500

    updated views for new model

--- kensoDataStore/views.py
+++ kensoDataStore/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from kensoDataStore.models import Tick
+from kensoDataStore.models import Volitility
 import simplejson as json
 import math
 
@@ -23,60 +24,14 @@ def temp_home(request):
 # Create your views here.
 def display_volatility(request):
 
-	symbol_one = request.GET.get("symbol1")
-	symbol_two = request.GET.get("symbol2")
+	symbol_one = request.GET.get("symbol")
 
-	date_one = int(request.GET.get("startdate").replace("/", ""))
-	date_two = int(request.GET.get("enddate").replace("/", ""))
-
-	listA = Tick.objects.filter(symbol = symbol_one, date__lte = date_two, date__gte = date_one).order_by("date")
-	listB = Tick.objects.filter(symbol = symbol_two, date__lte = date_two, date__gte = date_one).order_by("date")
-
-	lenA = len(listA)
-	lenB = len(listB)
-
-	if(lenA > lenB):
-		diff = lenA-lenB
-		listA = listA[diff:]
-		lenA -= diff
-	elif(lenB > lenA):
-		diff = lenB-lenA
-		listB = listB[diff:]
-		lenB -= diff
-
-	aSum, bSum = 0.0, 0.0
-	aCount, bCount = 0, 0
-	
-	# TODO - Why do we have 3 loops here? Should be done in pass. 
-	## Doesn't seem very maintainable or type-safe at first glance; should rewrite this section. 
-	# NOTE - This section will definitely break; Rewrite before moving to production
-	for a in listA:
-		aSum += float(a.percent_change)
-		aCount += 1
-	for b in listB:
-		bSum += float(b.percent_change)
-		bCount += 1
-	aAvg = aSum/aCount
-	bAvg = bSum/bCount
-	covNumerator = 0.0
-	varxNumerator, varyNumerator = 0.0, 0.0
-	for i in range(aCount): #acount and bcount should be equal
-		covNumerator += (float(listA[i].percent_change) - aAvg) * (float(listB[i].percent_change) - bAvg)
-		varxNumerator += math.pow((float(listA[i].percent_change) - aAvg),2)
-		varyNumerator += math.pow((float(listB[i].percent_change) - bAvg),2)
-	cov = covNumerator/aCount
-	varx = float(varxNumerator) / aCount
-	vary = float(varyNumerator) / bCount
-	sigmax = math.pow(varx,0.5)
-	sigmay = math.pow(vary,0.5)
-	correlation = cov/(sigmax*sigmay)
-	# END TODO; this code needs to be cleaned up. Execution is too slow. 
+	volitility_data = Volitility.objects.filter(symbol = symbol_one)
 
 	ret = {}
-	ret["correlation_%s_%s" % (symbol_one, symbol_two)] = {}
-	ret["correlation_%s_%s" % (symbol_one, symbol_two)]["correlation"] = correlation
-	ret["correlation_%s_%s" % (symbol_one, symbol_two)]["sentiment_%s" % symbol_one] = None
-	ret["correlation_%s_%s" % (symbol_one, symbol_two)]["sentiment_%s" % symbol_two] = None
+	ret[symbol_one] = {}
+	ret[symbol_one]["volitility"] = volitility_data.volitliity
+	ret[symbol_one]["sentiment"] = None
 
 	return HttpResponse(json.dumps(ret))
 
