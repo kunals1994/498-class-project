26d50d076294df9b7a7d6bdc55f092a70a82411d
Kunal Sharma <ksharma8@terpmail.umd.edu>
2014-12-05 20:36:16 -0500

    sentiment analys

--- kensoDataStore/views.py
+++ kensoDataStore/views.py
@@ -6,7 +6,7 @@ import simplejson as json
 import math
 
 import requests
-from alchemyapi import AlchemyAPI
+from django_project.alchemyapi.py import AlchemyAPI
 import json
 
 # Helper Method for determining Sentiment
